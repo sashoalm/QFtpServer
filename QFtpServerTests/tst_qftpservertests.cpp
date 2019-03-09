a19518582d8a8e619205d5e6e26a8aa50591915c
Alexander Almaleh <sashoalm@gmail.com>
2019-03-09 15:39:25 +0200

    Refactor, using common logic for the Arrange part.

--- QFtpServerTests/tst_qftpservertests.cpp
+++ QFtpServerTests/tst_qftpservertests.cpp
@@ -20,101 +20,113 @@ QFtpServerTests::QFtpServerTests()
 {
 }
 
-// Removes a folder and all its contents, recursively. We use this because
-// Qt4 lacks QDir::removeRecursively().
-// From https://stackoverflow.com/questions/27758573/deleting-a-folder-and-all-its-contents-with-qt/27758627
-bool removeRecursively(const QString & dirName)
+class Arrange
 {
-    bool result = true;
-    QDir dir(dirName);
+public:
+    struct Params
+    {
+        const char *userName;
+        const char *password;
+        bool login;
+    };
 
-    if (dir.exists(dirName)) {
-        Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst)) {
-            if (info.isDir()) {
-                result = removeRecursively(info.absoluteFilePath());
-            }
-            else {
-                result = QFile::remove(info.absoluteFilePath());
-            }
-
-            if (!result) {
-                return result;
-            }
+    Arrange(Params *params = 0)
+    {
+        Params defaultParams;
+        defaultParams.userName = "";
+        defaultParams.password = "";
+        defaultParams.login = true;
+        if (!params) {
+            params = &defaultParams;
         }
-        result = dir.rmdir(dirName);
+
+        int port = 9421;
+        rootPath = "/tmp/ftpservertest/";
+        removeRecursively(rootPath);
+        QDir().mkpath(rootPath);
+        server.reset(new FtpServer(0, rootPath, port, params->userName, params->password));
+
+        client.reset(new QFtp());
+        client->connectToHost("localhost", port);
+        if (params->login) {
+            client->login(params->userName, params->password);
+        }
+
+        loop.reset(new QEventLoop());
+        QObject::connect(client.data(), SIGNAL(done(bool)), loop.data(), SLOT(quit()));
     }
-    return result;
-}
+
+    // Removes a folder and all its contents, recursively. We use this because
+    // Qt4 lacks QDir::removeRecursively().
+    // From https://stackoverflow.com/questions/27758573/deleting-a-folder-and-all-its-contents-with-qt/27758627
+    static bool removeRecursively(const QString & dirName)
+    {
+        bool result = true;
+        QDir dir(dirName);
+
+        if (dir.exists(dirName)) {
+            Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst)) {
+                if (info.isDir()) {
+                    result = removeRecursively(info.absoluteFilePath());
+                }
+                else {
+                    result = QFile::remove(info.absoluteFilePath());
+                }
+
+                if (!result) {
+                    return result;
+                }
+            }
+            result = dir.rmdir(dirName);
+        }
+        return result;
+    }
+
+    QScopedPointer<FtpServer> server;
+    QScopedPointer<QFtp> client;
+    QScopedPointer<QEventLoop> loop;
+    QString rootPath;
+};
 
 void QFtpServerTests::testCaseMkd()
 {
     // Arrange
-    int port = 9421;
-    QString rootPath = "/tmp/ftpservertest/";
-    removeRecursively(rootPath);
-    QDir().mkpath(rootPath);
-    FtpServer server(this, rootPath, port);
-    Q_UNUSED(server);
+    Arrange a;
 
     // Act
-    QFtp client;
-    client.connectToHost("localhost", port);
-    client.login();
-    client.mkdir("foo");
-
-    // Wait for the server to finish its work. The
-    // ftp client emits a "done()" signal when the
-    // last pending command has finished.
-    QEventLoop loop;
-    connect(&client, SIGNAL(done(bool)), &loop, SLOT(quit()));
-    loop.exec();
+    a.client->mkdir("foo");
+    a.loop->exec();
 
     // Assert
-    QVERIFY(QDir(rootPath + "/foo").exists());
+    QVERIFY(QDir(a.rootPath + "/foo").exists());
 }
 
 void QFtpServerTests::testCaseIncorrectLogin()
 {
     // Arrange
-    int port = 9421;
-    QString rootPath = "/tmp/ftpservertest/";
-    removeRecursively(rootPath);
-    QDir().mkpath(rootPath);
-    FtpServer server(this, rootPath, port, "user1", "pass1");
-    Q_UNUSED(server);
-    QFtp client;
-    QEventLoop loop;
-    connect(&client, SIGNAL(done(bool)), &loop, SLOT(quit()));
+    Arrange::Params p = { "user1", "pass1", false };
+    Arrange a(&p);
 
     // Act
-    client.connectToHost("localhost", port);
-    client.login("user1", "pass2");
-    loop.exec();
+    a.client->login("user1", "pass2");
+    a.loop->exec();
 
     // Assert
-    QCOMPARE(client.error(), QFtp::UnknownError);
+    QCOMPARE(a.client->error(), QFtp::UnknownError);
 }
 
 void QFtpServerTests::testCaseCorrectLogin()
 {
     // Arrange
-    int port = 9421;
-    QString rootPath = "/tmp/ftpservertest/";
-    removeRecursively(rootPath);
-    QDir().mkpath(rootPath);
-    FtpServer server(this, rootPath, port, "user1", "pass1");
-    Q_UNUSED(server);
-    QFtp client;
-    QEventLoop loop;
-    connect(&client, SIGNAL(done(bool)), &loop, SLOT(quit()));
+    Arrange::Params p = { "user1", "pass1", false };
+    Arrange a(&p);
 
     // Act
-    client.connectToHost("localhost", port);
-    client.login("user1", "pass1");
-    loop.exec();
+    a.client->login("user1", "pass1");
+    a.loop->exec();
 
     // Assert
-    QCOMPARE(client.error(), QFtp::NoError);
+    QCOMPARE(a.client->error(), QFtp::NoError);
 }
 
 QTEST_MAIN(QFtpServerTests)
